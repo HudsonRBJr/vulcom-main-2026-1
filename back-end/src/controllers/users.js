adc1c54f246e55fca7ec0bfc1ceb5f46826e148c
HUDSON RIBEIRO BARBARA JÚNIOR <1091392413010@fatec-srv.local>
2026-05-09 10:54:57 -0300

    (09/05) Armazenamento do token JWT em cookie HTTP-Only e seguro

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -132,6 +132,9 @@ controller.login = async function(req, res) {
         }
       })
 
+
+      
+
       // Se o usuário não for encontrado, retorna
       // HTTP 401: Unauthorized
       if(! user) return res.status(401).end()
@@ -149,12 +152,30 @@ controller.login = async function(req, res) {
       // HTTP 401: Unauthorized
       if(! passwordIsValid) return res.status(401).end()
 
+     // Eliminamos o campo "password" dos dados do usuário antes de incluí-lo
+     // no payload do token JWT
+     if(user.password) delete user.password    // Eliminamos o campo "password" dos dados do usuário antes de incluí-lo
+     // no payload do token JWT
+     if(user.password) delete user.password
+
+
+
       // Usuário e senha OK, passamos ao procedimento de gerar o token
       const token = jwt.sign(
         user,                       // Dados do usuário
         process.env.TOKEN_SECRET,   // Senha para criptografar o token
         { expiresIn: '24h' }        // Prazo de validade do token
       )
+
+         // Formamos o cookie para enviar ao front-end
+     res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+      httpOnly: true, // O cookie ficará inacessível para o JS no front-end
+      secure: true,   // O cookie será criptografado em conexões https
+      sameSite: 'None',
+      path: '/',
+      maxAge: 24 * 60 * 60 * 100  // 24h
+    })
+
 
       // Formamos o cookie para enviar ao front-end
       res.cookie(process.env.AUTH_COOKIE_NAME, token, {
@@ -167,7 +188,7 @@ controller.login = async function(req, res) {
 
       // Retorna o token e o usuário autenticado com
       // HTTP 200: OK (implícito)
-      res.send({token, user})
+      res.send({user})
 
   }
   catch(error) {
@@ -178,10 +199,25 @@ controller.login = async function(req, res) {
   }
 }
 
+
+
+
 controller.me = function(req, res) {
   // Retorna as informações do usuário autenticado
   // HTTP 200: OK (implícito)
   res.send(req?.authUser)
 }
+
+controller.logout = function(req, res) {
+  // Apaga no front-end o cookie que armazena o token de autorização
+  res.clearCookie(process.env.AUTH_COOKIE_NAME, {
+    path: '/',
+    secure: true,
+    sameSite: 'None'
+  })
+  // HTTP 204: No Content
+  res.status(204).end()
+ }
+ 
 
 export default controller
--- front-end/src/ui/AuthControl.jsx
+++ front-end/src/ui/AuthControl.jsx
@@ -21,16 +21,22 @@ export default function AuthControl() {
 
   async function handleLogoutButtonClick() {
     if(await askForConfirmation('Deseja realmente sair?')) {
-      // Apaga o token do localStorage
-      window.localStorage.removeItem(import.meta.env.VITE_AUTH_TOKEN_NAME)
-
+      await myfetch.post('/users/logout')
+ 
+ 
+      // Apaga o token armazenado no localStorage
+      // window.localStorage.removeItem(import.meta.env.VITE_AUTH_TOKEN_NAME)
+ 
+ 
       // Remove as informações do usuário autenticado
       setAuthUser(null)
-
+ 
+ 
       // Redireciona para a página de login
       navigate('/login')
     }
   }
+ 
 
   if(authUser) {
     return (
